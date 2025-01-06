0b6fc8f4607dc828a80cd969ebb9d36571353a5d
Betül Korkut <krktbtl@gmail.com>
2025-01-06 16:56:55 +0300

    working on NICK

--- server/nick.cpp
+++ server/nick.cpp
@@ -8,13 +8,22 @@ void	Server::NICK(int fd, std::vector<std::string> params)
 {
 	if (params.size() < 2)
 		return ; // ERR_NONICKNAMEGIVEN
+
+	// needs several touppers everywhere
 	if (params[1].find_first_not_of(NICKSET) != std::string::npos)
 		return ; // ERR_ERRONEOUSNICKNAME
+	for (std::map<int, User>::const_iterator i = users.begin(); i != users.end(); ++i)
+	{
+		if (i->second.getNick() == params[1])
+			return ; // ERR_NICKNAMEINUSE
+	}
 
-	// check if nickname is already in use in a user (NICKNAMEINUSE OR NICKCOLLISION)
-	// if not, set or change the prev one
-	// if so, issue an ERR_NICKNAMEINUSE numeric and ignore nick
+	// save previous nick for reply
+	users[fd].setNick(params[1]);
+
 	std::cout << "Command from user:" << this->users[fd].getNick() << std::endl;
 	for (size_t i = 0; i < params.size(); i++)
 		std::cout << params[i] << std::endl;
+
+	return ; // :previous_nickname NICK params[1]
 }
